752e40b83c559ddf4de7631e1018eca5a68c189c
Fatih Yazicioglu <fy134388@gmail.com>
2023-03-28 13:38:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,8 +3,16 @@ self.addEventListener('install', function(event) {
     // Perform installation process
     event.waitUntil(
       caches.open("static").then(cache =>{
-        return cache.addAll([
+        return cache.addAll(
           "/", "./src/master.css", "./images/alex-shuper-JA14fUcNKFE-unsplash.jpg"
       })
     );
   })
+
+  self.addEventListener('fetch', function(event) {
+ console.log(´'Intercepting fetch request for: ' + ${event.request.url}´);
+  });
+  
+  
+  
+  
